19ba8fdf091ab5987769f344c3efb487313b834d
emmanuelkamala <emmanuelkamala86@gmail.com>
2020-12-02 00:59:42 +0300

    Add footer component to the app

---
+++ src/components/footer/styles/footer.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components/macro';
+
+export const Container = styled.div`
+  display: flex;
+  max-width: 1000px;
+  padding: 70px 56px;
+  margin: auto;
+  flex-direction: column;
+
+  @media (max-width: 1000px) {
+    padding: 70px 30px;
+  }
+`
+
+export const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: left;
+`
+
+export const Row = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(230px, 1fr));
+  grid-gap: 15px;
+
+  @media (max-width: 1000px) {
+    grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
+  }
+`
+
