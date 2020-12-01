42584abdbd5a11f5271cb5b3aef25ede4558a453
emmanuelkamala <emmanuelkamala86@gmail.com>
2020-12-01 17:37:18 +0300

    Style the compound components

--- src/components/jumbotron/Jumbotron.js
+++ src/components/jumbotron/Jumbotron.js
@@ -1,12 +1,34 @@
 import React from 'react';
-import { Inner } from './styles/JumbotronStyle';
+import { Container, Inner, Item, Pane, Title, SubTitle, Image } from './styles/JumbotronStyle';
 
 function Jumbotron({ children, direction= 'row', ...restProps}) {
   return (
-    <Inner direction={direction}>
-      I am Jumbotron
-    </Inner>
+    <Item direction={direction}>
+      <Inner>
+        {children}
+      </Inner>
+    </Item>
   )
 }
 
 export default Jumbotron;
+
+Jumbotron.Container = function JumbotronContainer({ children, ...restProps }){
+  return <Container {...restProps}>{children}</Container>
+}
+
+Jumbotron.Title = function JumbotronTitle({ children, ...restProps }){
+  return <Title {...restProps}>{children}</Title>
+}
+
+Jumbotron.SubTitle = function JumbotronSubTitle({ children, ...restProps }){
+  return <SubTitle {...restProps}>{children}</SubTitle>
+}
+
+Jumbotron.Pane = function JumbotronPane({ children, ...restProps }){
+  return <Pane {...restProps}>{children}</Pane>
+}
+
+Jumbotron.Image = function JumbotronImage({ ...restProps }){
+  return <Image {...restProps} />
+}
