55a8fbe6db1440daf16420570d0aa741ed3a60bf
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-08-15 01:18:33 +0300

    Add logo image to the app

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+import { ArrowDropDown, Notifications, Search } from '@material-ui/icons';
+import React from 'react';
+import './navbar.scss';
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+      <div className="container">
+        <div className="left">
+          <img src="./images/logo.png" alt="logo" />
+        </div>
+        <span>Homepage</span>
+        <span>Series</span>
+        <span>Movies</span>
+        <span>New and Popular</span>
+        <span>My List</span>
+        <div className="right">
+          <Search />
+          <span>KID</span>
+          <Notifications />
+          <img src="./images/users/1.png" alt="profile" />
+          <ArrowDropDown />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Navbar from '../../components/navbar/Navbar';
 import './home.scss';
 
 function Home() {
   return (
     <div className='home'>
-      hello
+      <Navbar />
     </div>
   )
 }
