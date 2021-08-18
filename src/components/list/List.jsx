459d0e9f2137462342dad9a0bada5e8b5ede4e28
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-08-18 15:37:45 +0300

    Add a video trailer to the movie item

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -34,26 +34,32 @@ function List() {
           style={{ display: !isMoved && "none" }}
         />
         <div className="container" ref={listRef}>
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
+          <ListItem index={0} />
+          <ListItem index={1} />
+          <ListItem index={2} />
+          <ListItem index={3} />
+          <ListItem index={4}/>
+          <ListItem index={5}/>
+          <ListItem index={6}/>
+          <ListItem index={7}/>
+          <ListItem index={8}/>
+          <ListItem index={9}/>
+          <ListItem index={10}/>
+          <ListItem index={11}/>
+          <ListItem index={12}/>
+          <ListItem index={13}/>
+          <ListItem index={14}/>
+          <ListItem index={15}/>
+          <ListItem index={16}/>
         </div>
         <ArrowForwardIosOutlined className="sliderArrow right" onClick={()=> handleClick("right")} />
       </div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, autem inventore labore laudantium assumenda doloremque. Sequi facere dolorem, ipsa maiores molestias deserunt hic quisquam, accusamus officiis, perspiciatis ab! Iste, porro.
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime placeat obcaecati necessitatibus recusandae quidem veniam ea dignissimos rerum dolorum asperiores, consequatur excepturi fugit amet molestiae commodi distinctio repellat praesentium natus!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint accusantium error molestias, enim ipsa earum neque doloribus, minima minus a odio impedit inventore perferendis eos nobis architecto quasi repellendus dolorem.
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex ad modi quisquam deserunt, perspiciatis odio quaerat! Amet aliquam perferendis veritatis totam, aliquid eaque quia aperiam tempora excepturi asperiores quidem sed.
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, natus illum fuga vel autem corrupti, quas illo eius minima porro excepturi voluptatum esse odio suscipit? Assumenda vel provident voluptas excepturi.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui atque ratione nostrum dolores sunt officia voluptates distinctio id quis deleniti ea praesentium cum, porro ullam pariatur fugit! Minus, nobis provident?
     </div>
   )
 }
--- src/components/listitem/ListItem.jsx
+++ src/components/listitem/ListItem.jsx
@@ -1,30 +1,45 @@
 import { PlayArrow, Add, ThumbUpAltOutlined, ThumbDownOutlined } from '@material-ui/icons';
-import React from 'react'
+import React, { useState } from 'react'
 import './listitem.scss';
 
-function ListItem() {
+function ListItem({index}) {
+  const [isHovered, setIsHovered] = useState(false);
+  const trailer = "./videos/bunny.mp4";
+
   return (
-    <div className="listitem">
+    <div className="listitem" 
+      style={{ left: isHovered && index * 225 - 50 + index * 2.5 }}
+      onMouseEnter={() => setIsHovered(true)} 
+      onMouseLeave={() => setIsHovered(false)}>
       <img src="./images/series/crime/long-shot/small.jpg" alt="series" />
-      <div className="itemsInfo">
-        <div className="icons">
-          <PlayArrow />
-          <Add />
-          <ThumbUpAltOutlined />
-          <ThumbDownOutlined />
-        </div>
-        <div className="itemInfoTop">
-          <span>1 hour 14 mins</span>
-          <span>+16</span>
-          <span>2020</span>
-        </div>
-        <div className="desc">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. error ipsa perferendis delectus cum inventore quod!
-        </div>
-        <div className="genre">
-          Action
-        </div>
-      </div>
+
+      {
+        isHovered && (
+          <>
+            <video src={trailer} autoPlay={true} loop />
+            <div className="itemsInfo">
+              <div className="icons">
+                <PlayArrow />
+                <Add />
+                <ThumbUpAltOutlined />
+                <ThumbDownOutlined />
+              </div>
+              <div className="itemInfoTop">
+                <span>1 hour 14 mins</span>
+                <span>+16</span>
+                <span>2020</span>
+              </div>
+              <div className="desc">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. error ipsa perferendis delectus cum inventore quod!
+              </div>
+              <div className="genre">
+                Action
+              </div>
+            </div>
+          </>
+        )
+      }
+      
     </div>
   )
 }
