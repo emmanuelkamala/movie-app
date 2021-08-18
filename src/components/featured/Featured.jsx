942408a5ca513ffbfec48659230292ef3ee796e9
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-08-18 04:05:36 +0300

    Create a list of genres

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -2,9 +2,33 @@ import { InfoOutlined, PlayArrow } from '@material-ui/icons';
 import React from 'react';
 import './featured.scss';
 
-function Featured() {
+function Featured({ type }) {
   return (
     <div className="featured">
+      {
+        type && (
+          <div className="category">
+            <span>{type === "movie" ? "Movies" : "Series"}</span>
+            <select name="genre" id="genre">
+              <option>Genre</option>
+              <option value="adventure">Adventure</option>
+              <option value="comedy">Comedy</option>
+              <option value="crime">Crime</option>
+              <option value="fantasy">Fantasy</option>
+              <option value="historical">Historical</option>
+              <option value="horror">Horror</option>
+              <option value="romance">Romance</option>
+              <option value="sci-fi">Sci-fi</option>
+              <option value="thriller">Thriller</option>
+              <option value="western">Western</option>
+              <option value="animation">Animation</option>
+              <option value="drama">Drama</option>
+              <option value="documentary">Documentary</option>
+
+            </select>
+          </div>
+        )
+      }
       <img src="./images/misc/home-bg-main.jpg" alt="featured" />
 
       <div className="info">
