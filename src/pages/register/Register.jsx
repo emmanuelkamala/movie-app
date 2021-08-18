4875bc5f02d947cb55b118898c3eefdbede386a0
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-08-19 02:35:33 +0300

    Add state to register component

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,8 +1,23 @@
 import { ArrowForwardOutlined } from '@material-ui/icons'
-import React from 'react'
+import React, { useRef } from 'react'
+import { useState } from 'react'
 import './register.scss'
 
 function Register() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  const handleClick = () => {
+    setEmail(emailRef.current.value)
+  }
+
+  const handleStart = () => {
+    setPassword(passwordRef.current.value)
+  }
+
   return (
     <div className="register" style={{backgroundImage: "url('./images/misc/home-bg.jpg')"}}>
       <div className="top">
@@ -15,10 +30,21 @@ function Register() {
          <h1 className="infoHeading">Unlimited movies, TV shows, and more.</h1>
         <h2 className="infoDesc">Watch anywhere. Cancel anytime.</h2>
         <p className="infoAction">Ready to watch? Enter your email to create or restart your membership.</p>
-        <div className="input">
-          <input type="email" placeholder="Email address" />
-          <button className="registerButton"><span className="infoButtonTitle">Get Started</span><ArrowForwardOutlined /></button>
-        </div>
+
+        {
+          !email ? (
+            <div className="input">
+              <input type="email" placeholder="Email address" ref={emailRef} />
+              <button className="registerButton" onClick={handleClick}>Get Started <ArrowForwardOutlined /></button>
+            </div>
+          ) : (
+            <form className="input">
+              <input type="password" placeholder="Password" ref={passwordRef} />
+              <button className="registerButton" onClick={handleStart}>Start </button>
+            </form>
+          )
+        }
+        
       </div>
     </div>
   )
