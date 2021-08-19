2687a2e30ef228b5aa70a4f430e799bebc392c0d
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-08-19 03:05:57 +0300

    Edit README file

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
-// import Home from "./pages/home/Home";
+import Home from "./pages/home/Home";
 import './app.scss';
-// import Register from './pages/register/Register';
-// import Watch from './pages/watch/Watch';
+import Register from './pages/register/Register';
+import Watch from './pages/watch/Watch';
 import Login from './pages/login/Login';
 
 function App() {
   return (
     <div>
-     {/* <Home /> 
+     <Home /> 
      <Watch /> 
-     <Register />*/}
+     <Register />
      <Login />
     </div>
   );
