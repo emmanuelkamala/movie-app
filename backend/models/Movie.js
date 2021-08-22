7f1ccc0794490301834672ba6ff20d58b734e1c5
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-08-22 08:18:28 +0300

    create a movie schema

---
+++ backend/models/Movie.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+const MovieSchema = new mongoose.Schema({
+  title: { type: String, required: true, unique },
+  description: { type: String },
+  img: { type: String },
+  imgTitle: { type: String },
+  imgSm: { type: String },
+  trailer: { type: String },
+  video: { type: String },
+  genre: { type: String },
+  limit: { type: Number },
+  year: { type: Number },
+  isSeries: { type: Boolean, default: false }
+},
+  { timestamps: true }
+)
+
+const movieSchema = new mongoose.model('Movie', MovieSchema);
+
+export default movieSchema;
