1978bbf3634cc50a38f1d36c480cd8197309ef6b
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-09-13 14:10:23 +0300

    Add a find single user route to the users resource

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -31,6 +31,7 @@ userRouter.put('/:id', verify, async (req, res) => {
 userRouter.delete('/:id', verify, async (req, res) => {
   if (req.user.id === req.params.id || req.user.isAdmin){
     try {
+      await User.findByIdAndDelete(req.params.id);
       res.status(200).json("User has been deleted");
     } catch (error) {
       res.status(500).json(err)
@@ -40,8 +41,16 @@ userRouter.delete('/:id', verify, async (req, res) => {
   }
 })
 
-
 // Get User
+userRouter.get('/find/:id', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, ...info } = user._doc;
+    res.status(200).json(info);
+  } catch (error) {
+    res.status(500).json(err)
+  }
+})
 
 // Get All Users
 
