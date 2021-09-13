e46d565cf9805014775b9cd2d5bc9b3734cf7829
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-09-13 16:59:13 +0300

    Create logic for updating movies

--- backend/routes/movies.js
+++ backend/routes/movies.js
@@ -6,7 +6,30 @@ const movieRouter = express.Router();
 // Create
 movieRouter.post('/:id', verify, async (req, res) => {
   if (req.user.isAdmin){
-    
+    const newMovie = new Movie(req.body);
+
+    try {
+      const savedMovie = await newMovie.save();
+      res.status(201).json(savedMovie);
+    } catch (error) {
+      res.status(500).json(error)
+    }
+  } else {
+    res.status(403).json("You are not allowed");
+  }
+})
+
+// Update
+movieRouter.put('/:id', verify, async (req, res) => {
+  if (req.user.isAdmin){
+    try {
+      const updatedMovie = await Movie.findByIdAndUpdate(req.params.id, {
+        $set: req.body,
+      }, { new: true });
+      res.status(200).json(updatedMovie);
+    } catch (error) {
+      res.status(500).json(error)
+    }
   } else {
     res.status(403).json("You are not allowed");
   }
