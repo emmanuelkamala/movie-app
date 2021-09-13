d872314b2517a308ea710dd35eb1bb809d53b66a
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-09-13 09:33:49 +0300

    Create a login method for user

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import User from '../models/User.js';
+import CryptoJS from 'crypto-js';
 const authRouter = express.Router();
+
 
 // Register
 
@@ -8,12 +10,32 @@ authRouter.post('/register', async (req, res) => {
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
-    password: req.body.password,
+    password: CryptoJS.AES.encrypt(req.body.password, process.env.SECRET_KEY).toString(),
   })
 
   try {
     const user = await newUser.save();
     res.status(201).json({ user, message: "User saved successfully"});
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+})
+
+// Login
+
+authRouter.post('/login', async(req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(404).json("Wrong password or username");
+
+    const bytes  = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
+    const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
+
+    if (originalPassword !== req.body.password){
+      return res.status(404).json("Wrong password or username")
+    } else {
+      return res.status(200).json(user)
+    }
   } catch (error) {
     res.status(500).json({ error: error.message })
   }
