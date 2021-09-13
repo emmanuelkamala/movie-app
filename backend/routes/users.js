75b705970d9c461833d317850026406a411280e6
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-09-13 14:15:27 +0300

    Get all users route

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -53,7 +53,19 @@ userRouter.get('/find/:id', async (req, res) => {
 })
 
 // Get All Users
-
+userRouter.get('/', verify, async (req, res) => {
+  const query = req.query.new;
+  if (req.user.isAdmin){
+    try {
+      const users = query ? await User.find().limit(10) : await User.find();
+      res.status(200).json(users);
+    } catch (error) {
+      res.status(500).json(err)
+    }
+  } else {
+    res.status(403).json("You are not allowed to see all users");
+  }
+})
 // Get Users Stats
 
 export default userRouter;
