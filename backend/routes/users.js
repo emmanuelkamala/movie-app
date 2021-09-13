dc0ced196d77bba1b4c3a91c8cc2b9cbf826267a
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-09-13 10:49:01 +0300

    Create a verify token file

---
+++ backend/routes/users.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import User from '../models/User.js';
+import CryptoJS from 'crypto-js';
+const userRouter = express.Router();
+
+// Update
+
+// Delete
+
+// Get User
+
+// Get All Users
+
+// Get Users Stats
